a74106a9598268b0a7f55c42ee662c4d084d2d78
Muneef M <mailmuneef@gmail.com>
2018-07-27 20:03:53 +0530

    Initial Commit

---
+++ Charger Alert/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Charger Alert
+//
+//  Created by Muneef m on 27/7/18.
+//  Copyright © 2018 Muneef m. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
