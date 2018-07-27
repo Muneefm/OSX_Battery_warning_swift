7b1550ead34b03e2174174fb85085b5290c459b4
Muneef M <mailmuneef@gmail.com>
2018-07-28 03:51:08 +0530

    Added Power notification loop

---
+++ Charger Alert/Battery.h
@@ -0,0 +1,13 @@
+//
+//  Battery.h
+//  Charger Alert
+//
+//  Created by Muneef m on 27/7/18.
+//  Copyright © 2018 Muneef m. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Battery : NSDocument
+
+@end
